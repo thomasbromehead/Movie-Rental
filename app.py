fe06374b6f876f29da06d62405e17cdd8bfb354d
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-12-13 16:09:44 +0100

    Deleting a movie with a filter+lambda

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from user import User
 
 my_movie = Movie("The Matrix", "sci-fi", False)
 user = User("Tom")
-user.movies.append(my_movie)
-print(user.watched_movies())
+user.add_movie("Dumb and Dumber", "comedy", False)
+user.add_movie("J", "comedy", False)
 user.movies[0].toggle_watched()
 print(user.watched_movies())
+user.delete_movie("J")
+print(user.movies)
 
 
--- user.py
+++ user.py
@@ -1,3 +1,6 @@
+from movie import Movie
+
+
 class User():
     def __init__(self, name):
         self.name = name
@@ -8,6 +11,13 @@ class User():
 
     def watched_movies(self):
         #Filter returns a filter object, turn it into a list
-        movies_watched = list(filter(lambda x: x.watched, self.movies))
-        return movies_watched
+        return list(filter(lambda movie: movie.watched, self.movies))
+
+
+    def add_movie(self, name, genre, watched):
+        movie = Movie(name, genre, watched)
+        self.movies.append(movie)
+
+    def delete_movie(self, name: object) -> object:
+        self.movies = list(filter(lambda movie: movie.name != name, self.movies))
 
